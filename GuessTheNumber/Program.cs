38010726103475cea1f5fdf9cc94b15fed1b8209
Sachin Gurung <saching061@hotmail.com>
2019-11-14 06:51:13 -0800

    First Commit

--- GuessTheNumber/Program.cs
+++ GuessTheNumber/Program.cs
@@ -10,6 +10,30 @@ namespace GuessTheNumber
     {
         static void Main(string[] args)
         {
+            var secretnumber = new Random().Next(1, 10);
+            Console.WriteLine("The secret number is: {0}", secretnumber);
+
+            for (var i = 0; i < 3; i++)
+            {
+                Console.Write("Guess the secret number: ");
+                var guessnumber = int.Parse(Console.ReadLine());
+
+                if (secretnumber == guessnumber)
+                {
+                    Console.WriteLine("You won");
+                    return;
+                }
+            }
+            Console.WriteLine("You lost");
         }
+
     }
 }
+/*
+The secret number is: 2
+Guess the secret number: 9
+Guess the secret number: 6
+Guess the secret number: 3
+You lost
+Press any key to continue . . .
+*/
